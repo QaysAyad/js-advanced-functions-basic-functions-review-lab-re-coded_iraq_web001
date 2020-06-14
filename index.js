c58e52a88e1f4e1e0ed508c639dff99289f563cc
Qays Ayad <work.worker55@gmail.com>
2020-06-14 10:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+let mondayWork = function(activity = "go to the office"){
+
+  return `This Monday, I will ${activity}.`
+}
+
+
+function wrapAdjective(visualFlair='*'){
+  return function (para1='special'){
+    return `You are ${visualFlair}${para1}${visualFlair}!`;
+
+  }
+}
